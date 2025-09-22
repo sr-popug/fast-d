8e3301fc07e9f2dca738f35cb591fc423fa45b77
sr-popug <antonpower2008@gmail.com>
2025-09-23 00:51:12 +0300

    fix 2

--- src/widgets/shop/Shop.tsx
+++ src/widgets/shop/Shop.tsx
@@ -1,9 +1,9 @@
-import styles from './shop.module.scss'
-import MyUpgrades from './ui/MyUpgrades/MyUpgrades'
-import NotBuyUpgrades from './ui/NotBuyUpgrades/NotBuyUpgrades'
+import styles from "./shop.module.scss"
+import MyUpgrades from "./ui/MyUpgrades/MyUpgrades"
+import NotBuyUpgrades from "./ui/NotBuyUpgrades/NotBuyUpgrades"
 export default function Shop() {
   return (
-    <article clac sName={styles.shop}>
+    <article className={styles.shop}>
       <h1>Магазин</h1>
       <MyUpgrades />
       <NotBuyUpgrades />
